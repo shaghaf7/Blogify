e12d1dfb348684e24314d9a2f4e13c5c64293df8
shaghaf7 <shaghafkashif@gmail.com>
2025-08-10 02:21:14 +0530

    added browser router

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App /> {/* ✅ Now Routes inside App will work */}
+    </BrowserRouter>
+  </React.StrictMode>
+);

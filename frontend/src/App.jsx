93954e07fdf02c4184e23566a581ea213367c879
shaghaf7 <shaghafkashif@gmail.com>
2025-08-12 01:09:50 +0530

    added the frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Register from './components/Register'
-import Login from './components/Login'
-import Allblogs from './components/Allblogs'
+import { useState } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Register from './components/Register';
+import Login from './components/Login';
+import Allblogs from './components/Allblogs';
+import AddBlog from './components/Addblog';
+import MyBlogs from './components/Myblogs';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Routes>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/allblogs' element={<Allblogs/>}/>
-    </Routes>
-     </>
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/allblogs' element={<Allblogs />} />
+        <Route path='/addblog' element={<AddBlog/>}/>
+        <Route path='/myblogs' element={<MyBlogs/>}/>
 
-  )
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
